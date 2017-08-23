42bbc4b75b9d7e6fd30f7263994df395de5af24c
xmdi <xmdi@protonmail.com>
2017-08-22 20:56:36 -0400

    relative paths

--- levi.py
+++ levi.py
@@ -1,9 +1,11 @@
 from random import randint
+import os
+fn=os.path.join(os.path.dirname(__file__),'levi.txt')
 a=1
-nl=sum(1 for line in open('levi.txt'))
+nl=sum(1 for line in open(fn))
 while a:
     wl=randint(0,nl-1)
-    with open('levi.txt') as f:
+    with open(fn) as f:
         l=f.readlines()
         if l[wl][0]=='0':
             vn=l[wl][0:7].split(':')
